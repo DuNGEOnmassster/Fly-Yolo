0b924aca20fc0aeaef024131c604fcf2e265b087
NormanZ <1275133384@qq.com>
2022-11-19 15:09:57 +0800

    upload anchor attempt

---
+++ Yolov1/anchor_test.py
@@ -0,0 +1,19 @@
+import torch
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Anchor attemption")
+
+    return parser.parse_args()
+
+
+def load_image():
+    pass
+
+
+def detect():
+    pass
+
+if __name__ == "__main__":
+    pass
