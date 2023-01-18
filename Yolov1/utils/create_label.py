daa83104106c4e470edb8d08ae40c4b044cff00a
tty <2650371826@qq.com>
2023-01-18 17:44:27 +0800

    2023-1-18 create_label.py finish

--- Yolov1/utils/create_label.py
+++ Yolov1/utils/create_label.py
@@ -26,7 +26,7 @@ create_targets: 制作全部样本
 """
 
 class CreateTargets():
-    def __init__(self, image_size, anchors, strides, labels):
+    def __init__(self, image_size, anchors, strides, labels, num_class):
         super(CreateTargets, self).__init__()
         """
         初始化
@@ -35,6 +35,8 @@ class CreateTargets():
         self.anchors = anchors
         self.strides = strides
         self.labels = labels
+        self.num_class = num_class
+        self.box_attr = 1 + 4 + 1 + num_class
     
     def set_anchors(self):
         anchors = np.array(self.anchors)
@@ -50,6 +52,8 @@ class CreateTargets():
     def build_target_without_anchors(self, target):
         stride_ind = 0
         anchor_ind = 0
+        position_target = []
+
         img_h, img_w = self.image_size
 
         batch_ind, _, nx, ny, nw, nh = target
@@ -59,15 +63,69 @@ class CreateTargets():
         grid_x = int(xc_s)
         grid_y = int(yc_s)
 
-        return [int(stride_ind), int(batch_ind), int(anchor_ind), grid_x, grid_y]
+        position_target.append([int(stride_ind), int(batch_ind), int(anchor_ind), grid_x, grid_y])
 
-    def build_target_with_anchors(self):
-        pass
+        return position_target
+
+    def build_target_with_anchors(self, target, num_anchors, multi_anchor=False):
+        batch_ind = target[0]
+        img_h, img_w = self.image_size
+
+        gt_box = torch.from_numpy(np.array([0, 0, target[4] * img_w, target[5] * img_h]))
+        anchor_boxes = self.set_anchors()
+
+        IoU = self.calculate_iou(gt_box.T, anchor_boxes, x1y1x2y2=False)
+
+        position_target = []
+        if multi_anchor:
+            # Truth table
+            IoU_mask = (IoU > 0.5)
+            print(IoU_mask)
+            if IoU_mask.sum() == 0:
+                IoU_ind = torch.argmax(IoU)
+                stride_ind = IoU_ind // num_anchors
+                anchor_ind = IoU_ind - stride_ind * num_anchors
+
+                xc_s = target[2] * img_w / self.strides[stride_ind]
+                yc_s = target[3] * img_h / self.strides[stride_ind]
+
+                grid_x = int(xc_s)
+                grid_y = int(yc_s)
+
+                position_target.append([int(stride_ind), int(batch_ind), int(anchor_ind), grid_x, grid_y])
+
+            else:
+                for IoU_ind, IoU_m in enumerate(IoU_mask):
+                    if IoU_m:
+                        stride_ind = IoU_ind // num_anchors
+                        anchor_ind = IoU_ind - stride_ind * num_anchors
+
+                        xc_s = target[2] * img_w / self.strides[stride_ind]
+                        yc_s = target[3] * img_h / self.strides[stride_ind]
+
+                        grid_x = int(xc_s)
+                        grid_y = int(yc_s)
+
+                        position_target.append([int(stride_ind), int(batch_ind), int(anchor_ind), grid_x, grid_y])
+        else:
+            IoU_ind = torch.argmax(IoU)
+            stride_ind = IoU_ind // num_anchors
+            anchor_ind = IoU_ind - stride_ind * num_anchors
+
+            xc_s = target[2] * img_w / self.strides[stride_ind]
+            yc_s = target[3] * img_h / self.strides[stride_ind]
+
+            grid_x = int(xc_s)
+            grid_y = int(yc_s)
+
+            position_target.append([int(stride_ind), int(batch_ind), int(anchor_ind), grid_x, grid_y])
+
+        return position_target
 
     def create_targets(self, center_sample=False):
 
         img_h, img_w = self.image_size
-        num_of_batch_ind = self.labels.shape[0]
+        batch_size = self.labels.shape[0]
         y_trues = []
         num_stride = len(self.strides)
         num_anchor_of_one_layer = len(self.anchors) // num_stride if self.anchors is not None else 1
@@ -75,52 +133,54 @@ class CreateTargets():
         for s in self.strides:
             fmp_w = img_w // s
             fmp_h = img_h // s
-            y_trues.append(np.zeros([num_of_batch_ind, num_anchor_of_one_layer, fmp_h, fmp_w, 1+1+4+1]))
+            y_trues.append(np.zeros([batch_size, num_anchor_of_one_layer, fmp_h, fmp_w, self.box_attr]))
 
         for i, label in enumerate(self.labels):
             label = label.numpy()
-            cls_ind = label[1]
+            cls_ind = int(label[1])
             # 平衡因子box_scale: 小框给大权重，大框给小权重
-            box_scale = 2.0 - (label[4] / img_w) * (label[5] / img_h)
-            position_target = []
+            box_scale = 2.0 - label[4] * label[5]
+
             if self.anchors is not None:
-                pass
+                position_target = self.build_target_with_anchors(label, num_anchors=num_anchor_of_one_layer, multi_anchor=True)
             else:
                 position_target = self.build_target_without_anchors(label)
 
-            stride_ind, batch_ind, anchor_ind, grid_x, grid_y = position_target
+            for position in position_target:
+                stride_ind, batch_ind, anchor_ind, grid_x, grid_y = position
+                print(position)
 
-            if center_sample:
-                # We consider four grid points near the center point
-                for j in range(grid_y, grid_y + 2):
-                    for i in range(grid_x, grid_x + 2):
-                        if (j >= 0 and j < y_trues[stride_ind].shape[2]) and (
-                                i >= 0 and i < y_trues[stride_ind].shape[3]):
-                            # confidence
-                            y_trues[stride_ind][batch_ind, anchor_ind, j, i, 0] = 1.0
-                            # class_ind
-                            y_trues[stride_ind][batch_ind, anchor_ind, j, i, 1] = cls_ind
-                            # nx+ny+nw+nh
-                            y_trues[stride_ind][batch_ind, anchor_ind, j, i, 2:6] = label[2:]
-                            # box_scale
-                            y_trues[stride_ind][batch_ind, anchor_ind, j, i, 6] = box_scale
-            else:
-                # We only consider top-left grid point near the center point
-                if (grid_y >= 0 and grid_y < y_trues[stride_ind].shape[2]) and (
-                        grid_x >= 0 and grid_x < y_trues[stride_ind].shape[3]):
-                    # confidence
-                    y_trues[stride_ind][batch_ind, anchor_ind, grid_y, grid_x, 0] = 1.0
-                    # class_ind
-                    y_trues[stride_ind][batch_ind, anchor_ind, grid_y, grid_x, 1] = cls_ind
-                    # nx+ny+nw+nh
-                    y_trues[stride_ind][batch_ind, anchor_ind, grid_y, grid_x, 2:6] = label[2:]
-                    # box_scale
-                    y_trues[stride_ind][batch_ind, anchor_ind, grid_y, grid_x, 6] = box_scale
+                if center_sample:
+                    # We consider four grid points near the center point
+                    for j in range(grid_y, grid_y + 2):
+                        for i in range(grid_x, grid_x + 2):
+                            if (j >= 0 and j < y_trues[stride_ind].shape[2]) and (
+                                    i >= 0 and i < y_trues[stride_ind].shape[3]):
+                                # confidence
+                                y_trues[stride_ind][batch_ind, anchor_ind, j, i, 0] = 1.0
+                                # nx+ny+nw+nh
+                                y_trues[stride_ind][batch_ind, anchor_ind, j, i, 1:5] = label[2:]
+                                # box_scale
+                                y_trues[stride_ind][batch_ind, anchor_ind, j, i, 5] = box_scale
+                                # cls_ind
+                                y_trues[stride_ind][batch_ind, anchor_ind, j, i, cls_ind + 6] = 1
+                else:
+                    # We only consider top-left grid point near the center point
+                    if (grid_y >= 0 and grid_y < y_trues[stride_ind].shape[2]) and (
+                            grid_x >= 0 and grid_x < y_trues[stride_ind].shape[3]):
+                        # confidence
+                        y_trues[stride_ind][batch_ind, anchor_ind, grid_y, grid_x, 0] = 1
+                        # class_ind
+                        y_trues[stride_ind][batch_ind, anchor_ind, grid_y, grid_x, 1:5] = label[2:]
+                        # nx+ny+nw+nh
+                        y_trues[stride_ind][batch_ind, anchor_ind, grid_y, grid_x, 5] = box_scale
+                        # box_scale
+                        y_trues[stride_ind][batch_ind, anchor_ind, grid_y, grid_x, cls_ind + 6] = 1
 
-            y_trues = [y_true.reshape(num_of_batch_ind, -1, 1 + 1 + 4 + 1) for y_true in y_trues]
-            y_trues = np.concatenate(y_trues, axis=1)
+        y_trues = [y_true.reshape(batch_size, -1, self.box_attr) for y_true in y_trues]
+        y_trues = np.concatenate(y_trues, axis=1)
 
-            return torch.from_numpy(y_trues).float()
+        return torch.from_numpy(y_trues).float()
 
 if __name__ == "__main__":
     anchors = [   [10, 13],   [16, 30],   [33, 23],
@@ -129,16 +189,29 @@ if __name__ == "__main__":
     anchor_strides = [8, 16, 32]
     no_anchor_strides = [32]
     target_wh = torch.tensor([0, 0, 30, 30])
-    target = torch.tensor([[0, 0, 0.5, 0.5, 0.1, 0.1]])
+    target1 = torch.tensor([[0, 0, 0.5, 0.5, 0.1, 0.1],
+                            [0, 0, 0.6, 0.6, 0.1, 0.1],
+                            [1, 0, 0.6, 0.4, 0.3, 0.3]])
+    target2 = torch.tensor([[0, 0, 0.5, 0.5, 0.1, 0.1]])
 
-    create_targets = CreateTargets(image_size=(320, 320), anchors=None, strides=no_anchor_strides, labels=target)
+    # create_targets = CreateTargets(image_size=(320, 320), anchors=None, strides=no_anchor_strides, labels=target1)
+    # y_trues1 = create_targets.create_targets(center_sample=True)
+    # print(y_trues1.shape, y_trues1[y_trues1[..., 0] == 1.0])
 
+    # create_targets = CreateTargets(image_size=(320, 320), anchors=None, strides=no_anchor_strides, labels=target2)
+    # y_trues2 = create_targets.create_targets(center_sample=True)
+    # print(y_trues2.shape, y_trues2[y_trues2[..., 0] == 1.0])
+
+    create_targets = CreateTargets(image_size=(320, 320), anchors=anchors, strides=anchor_strides, labels=target2, num_class=20)
+    y_trues2_anchors = create_targets.create_targets(center_sample=False)
+    # 6300 = 40*40*3 + 20*20*3 + 10*10*3
+    print(y_trues2_anchors.shape, y_trues2_anchors[y_trues2_anchors[..., 0] == 1.0])
     # temp_anchors = create_targets.set_anchors()
     # iou = create_targets.calculate_iou(target_wh, temp_anchors, x1y1x2y2=False)
-    y_trues = create_targets.create_targets(center_sample=True)
-    print(y_trues, y_trues.shape, y_trues[y_trues[..., 0] == 1.0])
+    # print(iou)
+    # iou_mask = iou > 0.5
+    # print(iou_mask)
 
     # print(temp_anchors, temp_anchors.shape, temp_anchors.dtype)
     # print(iou)
-    print(target, target.shape)
-
+    # print(target, target.shape)
