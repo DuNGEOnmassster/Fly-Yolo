17467dd71af5bbcc0493bf91f4419cb87d552369
NormanZ <1275133384@qq.com>
2023-02-01 23:27:57 +0800

    prepare for train update in the other day

--- Yolov1/train.py
+++ Yolov1/train.py
@@ -83,88 +83,88 @@ def train():
     print(test_loader.dataset.dataset, "\nLength of test loader: ", len(test_loader))
     print(valid_loader.dataset[1][1])
     print(test_loader.dataset[1][1])
-    # # tricks init
-    # criterion = nn.CrossEntropyLoss()
-    # # early_stopping = EarlyStopping(args.patient, verbose=False)
-    # base_lr = args.lr
-    # tmp_lr = base_lr
-    # optimizer = optim.SGD(model.parameters(), 
-    #                         lr=args.lr, 
-    #                         momentum=args.momentum,
-    #                         weight_decay=args.weight_decay
-    #                         )
-    # clr = CosineAnnealingLR(optimizer, T_max=15000)
-    # # 
-    # if args.multi_scale: # for example
-    #     print('use the multi-scale trick ...')
-    #     train_size = [640, 640]
-    #     val_size = [416, 416]
-    # else:
-    #     train_size = [416, 416]
-    #     val_size = [416, 416]
+    # tricks init
+    criterion = nn.CrossEntropyLoss()
+    # early_stopping = EarlyStopping(args.patient, verbose=False)
+    base_lr = args.lr
+    tmp_lr = base_lr
+    optimizer = optim.SGD(model.parameters(), 
+                            lr=args.lr, 
+                            momentum=args.momentum,
+                            weight_decay=args.weight_decay
+                            )
+    clr = CosineAnnealingLR(optimizer, T_max=15000)
+    # 
+    if args.multi_scale: # for example
+        print('use the multi-scale trick ...')
+        train_size = [640, 640]
+        val_size = [416, 416]
+    else:
+        train_size = [416, 416]
+        val_size = [416, 416]
     
-    # # train
-    # for epoch in args.epoch:
-    #     start_time = time.time()
-    #     # use cos lr
-    #     if args.cos and epoch > 20 and epoch <= args.epoch - 20:
-    #         # use cos lr
-    #         tmp_lr = 0.00001 + 0.5*(base_lr-0.00001)*(1+math.cos(math.pi*(epoch-20)*1./ (args.epoch-20)))
-    #         set_lr(optimizer, tmp_lr)
+    # train
+    for epoch in args.epoch:
+        start_time = time.time()
+        # use cos lr
+        if args.cos and epoch > 20 and epoch <= args.epoch - 20:
+            # use cos lr
+            tmp_lr = 0.00001 + 0.5*(base_lr-0.00001)*(1+math.cos(math.pi*(epoch-20)*1./ (args.epoch-20)))
+            set_lr(optimizer, tmp_lr)
 
-    #     elif args.cos and epoch > args.epoch - 20:
-    #         tmp_lr = 0.00001
-    #         set_lr(optimizer, tmp_lr)
+        elif args.cos and epoch > args.epoch - 20:
+            tmp_lr = 0.00001
+            set_lr(optimizer, tmp_lr)
         
-    #     # use step lr
-    #     else:
-    #         tmp_lr = base_lr
+        # use step lr
+        else:
+            tmp_lr = base_lr
 
-    #     for iter_i, (images, targets) in enumerate(dataloader):
-    #         # WarmUp strategy for learning rate
-    #         if not args.no_warm_up:
-    #             if epoch < args.wp_epoch:
-    #                 tmp_lr = base_lr * pow((iter_i+epoch*epoch_size)*1. / (args.wp_epoch*epoch_size), 4)
-    #                 # tmp_lr = 1e-6 + (base_lr-1e-6) * (iter_i+epoch*epoch_size) / (epoch_size * (args.wp_epoch))
-    #                 set_lr(optimizer, tmp_lr)
+        for iter_i, (images, targets) in enumerate(dataloader):
+            # WarmUp strategy for learning rate
+            if not args.no_warm_up:
+                if epoch < args.wp_epoch:
+                    tmp_lr = base_lr * pow((iter_i+epoch*epoch_size)*1. / (args.wp_epoch*epoch_size), 4)
+                    # tmp_lr = 1e-6 + (base_lr-1e-6) * (iter_i+epoch*epoch_size) / (epoch_size * (args.wp_epoch))
+                    set_lr(optimizer, tmp_lr)
 
-    #             elif epoch == args.wp_epoch and iter_i == 0:
-    #                 tmp_lr = base_lr
-    #                 set_lr(optimizer, tmp_lr)
+                elif epoch == args.wp_epoch and iter_i == 0:
+                    tmp_lr = base_lr
+                    set_lr(optimizer, tmp_lr)
         
-    #         # to device
-    #         images = images.to(device)
+            # to device
+            images = images.to(device)
 
-    #         # multi-scale trick
-    #         if iter_i % 10 == 0 and iter_i > 0 and args.multi_scale:
-    #             # randomly choose a new size
-    #             size = random.randint(10, 19) * 32
-    #             train_size = [size, size]
-    #             model.set_grid(train_size)
-    #         if args.multi_scale:
-    #             # interpolate
-    #             images = torch.nn.functional.interpolate(images, size=train_size, mode='bilinear', align_corners=False)
+            # multi-scale trick
+            if iter_i % 10 == 0 and iter_i > 0 and args.multi_scale:
+                # randomly choose a new size
+                size = random.randint(10, 19) * 32
+                train_size = [size, size]
+                model.set_grid(train_size)
+            if args.multi_scale:
+                # interpolate
+                images = torch.nn.functional.interpolate(images, size=train_size, mode='bilinear', align_corners=False)
         
-    #         # make train label
-    #         # 存疑！！！
-    #         targets = [label.tolist() for label in targets]
-    #         targets = tools.gt_creator(input_size=train_size, stride=model.stride, label_lists=targets)
-    #         targets = torch.tensor(targets).float().to(device)
+            # make train label
+            # 存疑！！！
+            targets = [label.tolist() for label in targets]
+            targets = tools.gt_creator(input_size=train_size, stride=model.stride, label_lists=targets)
+            targets = torch.tensor(targets).float().to(device)
             
-    #         # forward and loss
-    #         conf_loss, cls_loss, txtytwth_loss, total_loss = model(images, target=targets)
+            # forward and loss
+            conf_loss, cls_loss, txtytwth_loss, total_loss = model(images, target=targets)
 
-    #         # backprop
-    #         total_loss.backward()        
-    #         optimizer.step()
-    #         optimizer.zero_grad()
+            # backprop
+            total_loss.backward()        
+            optimizer.step()
+            optimizer.zero_grad()
 
-    #         # save model
-    #         if (epoch + 1) % 10 == 0:
-    #             print('Saving state, epoch:', epoch + 1)
-    #             torch.save(model.state_dict(), os.path.join(args.m, 
-    #                     'yolo_' + repr(epoch + 1) + '.pth')
-    #                     ) 
+            # save model
+            if (epoch + 1) % 10 == 0:
+                print('Saving state, epoch:', epoch + 1)
+                torch.save(model.state_dict(), os.path.join(args.m, 
+                        'yolo_' + repr(epoch + 1) + '.pth')
+                        ) 
 
 
 if __name__ == "__main__":
